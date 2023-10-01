eec17c364ad386a4fea44273af5f6ad7230ff2d3
Emanuelle Ferreira <emaf213@gmail.com>
2023-10-01 15:57:31 -0300

    Primera version estable, sin fecha de nacimiento

--- prjN3/AppFamiliares/models.py
+++ prjN3/AppFamiliares/models.py
@@ -1,13 +1,17 @@
 from django.db import models
-from datetime import datetime as dt
+import datetime as dt
 
 # Create your models here.
 class Familiar(models.Model):
-    nombre = models.CharField(max_length=30)
-    edad = models.IntegerField()
+    first_name = models.CharField(max_length=14, default="")
+    last_name = models.CharField(max_length=14, default="")
+    age = models.IntegerField(default=0)
+    #birth_date = models.DateField(default=dt.date(1970,1,1))
+    address = models.CharField(max_length=255, default="")
+    is_alive = models.BooleanField(default=True)
     #f_nacimiento = models.DateField()
 
     def __str__(self):
-        return f"Nombre: \"{self.nombre}\", Edad: {self.edad}"
+        return f"Nombre: \"{self.first_name} {self.last_name}\", Edad: {self.age}"
 
 
--- prjN3/AppFamiliares/views.py
+++ prjN3/AppFamiliares/views.py
@@ -1,15 +1,17 @@
 #from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from AppFamiliares.models import Familiar
 
 # Create your views here.
-def listar_familiares(request, familiar):
+def listar_familiares(request):
+    context = { "misFamiliares": Familiar.objects.all() }
     plantilla = loader.get_template("familiares_v1.html")
 
     # for i in Familiar.objects.all():
     #     print(i.name)
 
     # Terminamos de construír el template renderizándolo con su contexto
-    documento = plantilla.render(familiar)
+    documento = plantilla.render(context)
 
     return HttpResponse(documento)
