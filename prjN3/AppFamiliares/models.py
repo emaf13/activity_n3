2cded24b88a9a9dda452877a9d774f05ebdfd10e
Emanuelle Ferreira <emaf213@gmail.com>
2023-10-02 21:46:56 -0300

    agregamos fecha de nacimiento como atributo de la clase Familiar

--- prjN3/AppFamiliares/models.py
+++ prjN3/AppFamiliares/models.py
@@ -1,17 +1,16 @@
 from django.db import models
-import datetime as dt
+from datetime import date
 
 # Create your models here.
 class Familiar(models.Model):
     first_name = models.CharField(max_length=14, default="")
     last_name = models.CharField(max_length=14, default="")
     age = models.IntegerField(default=0)
-    #birth_date = models.DateField(default=dt.date(1970,1,1))
+    birth_date = models.DateField(default=date.today())
     address = models.CharField(max_length=255, default="")
     is_alive = models.BooleanField(default=True)
-    #f_nacimiento = models.DateField()
 
     def __str__(self):
-        return f"Nombre: \"{self.first_name} {self.last_name}\", Edad: {self.age}"
+        return f"\"{self.first_name} {self.last_name}\""
 
 
